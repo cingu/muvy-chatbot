357281bed2f61110f59d3ac9d9c32c7ef72c2324
cingu <32195293+cingu@users.noreply.github.com>
2017-10-04 13:59:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ function sendMessage(event) {
 
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: EAAVYnwdT98YBAM9nYXkELlPlM1tAszeZB60eRBZAU1Q7hCSV1UHjo52NUvLIrQ9Iba9OlxPIuNGqZAEJJvxtrIM05ULQzNotFZAG26hEPhe8oGy38HEoVxasBOofsQTCFJMW5SnWehQ08YWMfsNjDTt5akBytmV1ZCZAj49J1WgwZDZD}, /********* */
+    qs: {access_token: FB_ACCESS_TOKEN}, /********* */
     method: 'POST',
     json: {
       recipient: {id: sender},
